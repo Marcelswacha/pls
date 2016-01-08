9f04d51585a380a7e3bef3327d1add638db66f2a
Marcel Swacha <m.swacha@wb.com.pl>
2016-01-08 16:47:21 +0100

    Cleaning up

--- main.c
+++ main.c
@@ -1,85 +1,7 @@
-#include <stdio.h>
-#include <dirent.h>
-#include <getopt.h>
-#include <libgen.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <sys/stat.h>
+#include <getopt.h>
 
 #include "node.h"
-#include "colors.h"
-#include "concat.h"
-
-static int no_dot = 0;
-
-uint64_t get_size(const char* path)
-{
-    struct stat buf;
-    struct dirent **namelist;
-    uint64_t size = 0u;
-    char* absolute_path;
-    int i, n;
-
-    lstat(path, &buf);
-    size += (unsigned long long )buf.st_size;
-    if ((S_ISDIR(buf.st_mode)) && (is_dot(path) == 0)) {
-        /* enter directory and get size of each node */
-        n = scandir(path, &namelist, 0, 0);
-        if (n < 0) {
-            perror("scandir");
-            printf(ANSI_COLOR_RED "%s" ANSI_COLOR_RESET"\n", path);
-        } else {
-            for (i = 0; i < n; ++i) {
-                /* get absolute path */
-                absolute_path = concat(path, "/", namelist[i]->d_name, NULL);
-
-                /* count the size */
-                size += get_size(absolute_path);
-
-                /* clean up */
-                free(namelist[i]);
-                free(absolute_path);
-            }
-            free(namelist);
-        }
-    }
-
-    return size;
-}
-
-void decorate(const char* parent, const char* child)
-{
-    /* get absolute path */
-    char* absolute_path = concat(parent, "/", child, NULL);
-
-    struct stat buf;
-
-    lstat(absolute_path, &buf);
-    uint64_t size = get_size(absolute_path);
-
-    if (S_ISDIR(buf.st_mode))
-        printf(ANSI_COLOR_CYAN "\t%-40s%llu" ANSI_COLOR_RESET"\n",
-                        child, size);
-    else
-        printf("\t%-40s%llu\n", child, size);
-
-    free(absolute_path);
-}
-
-int no_dot_filter(const struct dirent* entry)
-{
-    return (entry->d_name[0] == '.') ? 0 : 1;
-}
-
-int filter_collection(const struct dirent* entry)
-{
-    int result = 1;
-    if (no_dot) {
-        result = no_dot_filter(entry);
-    }
-
-    return result;
-}
 
 int listdir(const char* path)
 {
@@ -93,6 +15,7 @@ int listdir(const char* path)
     return 0;
 }
 
+static int no_dot = 0;
 static struct option long_options[] = {
     {"no-dot", 0, NULL, 'd'},
     {NULL, 0, NULL, 0}
